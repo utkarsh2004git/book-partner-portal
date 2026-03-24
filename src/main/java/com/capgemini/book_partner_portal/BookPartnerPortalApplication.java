96b8c93b471bde1c2acb370197454e8c3e96f3aa
prateekmishra-0 <mprateek580@gmail.com>
2026-03-24 15:40:24 +0530

    Initial commit: Spring Data REST skeleton

---
+++ src/main/java/com/capgemini/book_partner_portal/BookPartnerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.book_partner_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPartnerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPartnerPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/book_partner_portal/BookPartnerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.book_partner_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPartnerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
