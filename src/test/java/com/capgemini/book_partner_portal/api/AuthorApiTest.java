f451b98565b3f67fa2ed7e883fd728647c69982d
Utkarsh Shahare <utkarshshahare2004@gmail.com>
2026-03-26 10:51:31 +0530

    Refactor folder structure for tests

--- src/test/java/com/capgemini/book_partner_portal/api/AuthorApiTest.java
+++ src/test/java/com/capgemini/book_partner_portal/api/AuthorApiTest.java
@@ -1,4 +1,4 @@
-package com.capgemini.book_partner_portal.authorTest;
+package com.capgemini.book_partner_portal.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
