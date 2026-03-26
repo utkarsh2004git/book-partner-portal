cdf0ade6a03dbd3eff84d0ee6276e0ab79c90ca3
Utkarsh Shahare <utkarshshahare2004@gmail.com>
2026-03-26 15:51:11 +0530

    Add search methods for last name, city, phone, and state in AuthorRepository; enhance AuthorApiTest and AuthorRepositoryTest with corresponding tests

--- src/main/java/com/capgemini/book_partner_portal/repository/AuthorRepository.java
+++ src/main/java/com/capgemini/book_partner_portal/repository/AuthorRepository.java
@@ -1,10 +1,30 @@
 package com.capgemini.book_partner_portal.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.capgemini.book_partner_portal.entity.Author;
 
 @RepositoryRestResource
 public interface AuthorRepository extends JpaRepository<Author, String> {
 
+    @RestResource(path="firstname")
+    List<Author> findByFirstNameContainingIgnoreCase(String firstName);
+    
+    @RestResource(path="lastname")
+    List<Author> findByLastNameContainingIgnoreCase(String lastName);
+
+    @RestResource(path="city")
+    List<Author> findByCityIgnoreCase(String city);
+
+    @RestResource(path="phone")
+    List<Author> findByPhone(String phone);
+
+    @RestResource(path="state")
+    List<Author> findByStateIgnoreCase(String state);
+
+
+    
 }
